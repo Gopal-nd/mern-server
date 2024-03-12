c28e40ce46d49601a2f45e80a0d3994e845c857d
Gopal N D <gopalhelpyou@gmail.com>
2024-03-12 22:09:20 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import mongose from "mongoose"
+
+import { userRouter } from "./routes/users.js"
+import { recipeRouter } from "./routes/Recipe.js"
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.get("/",(req,res)=>{
+    res.json("hello world ")
+})
+app.use('/auth',userRouter);
+app.use('/recipes',recipeRouter)
+
+mongose.connect("mongodb+srv://gopalnd999:gopalnd999@cluster0.j9fzffu.mongodb.net/cluster0")
+
+app.listen(3001,()=>console.log("server Strted 3001"))
+
+
